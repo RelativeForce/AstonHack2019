81976d1353f1570fc01d3991471a88fc712c29b3
RelativeForce <joshiceddy@hotmail.com>
2019-11-02 20:57:19 +0000

    Added big goose

---
+++ Assets/BossGooseLogic.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BossGooseLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _goose;
+    [SerializeField]
+    private AudioSource _audio;
+
+    private Animator _animator;
+    private bool _isAlive;
+    private bool _isEnraged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _isAlive = true;
+        _isEnraged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isAlive)
+        {
+            if (!_isEnraged)
+            {
+                var aliveGeese = GameObject.FindGameObjectsWithTag("Small Goose")
+                    .Select(o => o.GetComponent<Animator>())
+                    .Any(a => !a.GetBool("isDead"));
+
+                if (!aliveGeese)
+                {
+                    _animator.SetBool("isEnraged", true);
+                    _isEnraged = true;
+                    _audio.Play();
+                }
+            }
+            else
+            {
+                var player = GameObject.FindGameObjectsWithTag("Player").First();
+
+                var playerPosition = player.transform.position;
+
+                var velocity = (playerPosition - _goose.position) / 100;
+
+                velocity.y = 0;
+
+                _goose.position += velocity;
+            }
+        }
+    }
+}
--- Assets/GooseLogic.cs
+++ Assets/GooseLogic.cs
@@ -1,23 +1,105 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class GooseLogic : MonoBehaviour
 {
+    public const float WalkSpeed = 0.03f;
+    public const float Boundary = 5f;
+    public static Random random = new Random();
+
     [SerializeField]
     private Transform _goose;
+    [SerializeField]
+    private AudioSource _audio;
 
-    public const float walkSpeed = 0.03f;
+    private Vector3 _velocity;
+    private bool _isAlive;
+    private Animator _animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        SetVelocity(RandomVelocity() * Direction(), RandomVelocity() * Direction());
+        _isAlive = true;
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _goose.position = new Vector3(_goose.position.x + walkSpeed, _goose.position.y, _goose.position.z);
+        if (_isAlive)
+        {
+            float xVelocity = FixVelocity(_velocity.x, _goose.position.x);
+
+            float zVelocity = FixVelocity(_velocity.z, _goose.position.z);
+
+            SetVelocity(xVelocity, zVelocity);
+
+            UpdateRotation();
+
+            UpdatePosition();
+        }
+    }
+
+    private static float FixVelocity(float currentVelocity, float currentPos)
+    {
+        if (Math.Abs(currentPos + currentVelocity) < Boundary)
+        {
+            return currentVelocity;
+        }
+
+        var v = RandomVelocity();
+
+        if (currentVelocity > 0)
+        {
+            return v  * -1;
+        }
+
+        return v;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bat" && _isAlive)
+        {
+            _isAlive = false;
+            _animator.SetBool("isDead", true);
+            _goose.rotation = Quaternion.Euler(0, 90 * random.Next(0, 3), 90);
+            _audio.Play();
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        _goose.position = new Vector3(_goose.position.x + _velocity.x, _goose.position.y, _goose.position.z + _velocity.z);
+    }
+
+    private void UpdateRotation()
+    {
+        var angle = (float) (((Math.Atan(_velocity.x / _velocity.z) * 180) / Math.PI )) + 180;
+
+        _goose.rotation = Quaternion.Euler(0, angle, 0);
+
+    }
+
+    private void SetVelocity(float x, float z)
+    {
+        _velocity = new Vector3(x, 0f, z);
+    }
+
+    private static int Direction()
+    {
+        return (random.Next(1) == 1 ? -1 : 1);
+    }
+
+    private static float RandomVelocity()
+    {
+        var v = random.NextDouble() * WalkSpeed;
+
+        return (float) v;
     }
 }
